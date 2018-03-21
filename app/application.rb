2eb16f51350c33ee71801f6f0300e5ddc152f4af
Sean Oughton <seanoughton@gmail.com>
2018-03-21 19:13:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,8 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif requ.path.match(/items)
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
